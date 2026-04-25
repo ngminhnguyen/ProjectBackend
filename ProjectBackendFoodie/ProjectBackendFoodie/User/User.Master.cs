42c2c5fa6e7ccb18adc7938d735d528083073abe
ngminhnguyen <minhnguyen2811200909@gmail.com>
2026-04-25 21:50:12 +0700

    Fontend

---
+++ ProjectBackendFoodie/ProjectBackendFoodie/User/User.Master.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ProjectBackendFoodie.User
+{
+    public partial class User : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Request.Url.AbsoluteUri.ToString().Contains("Default.aspx"))
+            {
+                
+            }
+            else
+            {
+                form1.Attributes.Remove("class");
+
+                //load the control
+                Control sliderUserControl = (Control)Page.LoadControl("SliderUserControl.ascx");
+
+                //Add the control to the panel
+               pnlSliderUC.Controls.Add(sliderUserControl);
+
+            }
+
+            if (Session["userId"] != null)
+            {
+                lbLoginOrLogout.Text = "Logout";
+                Utils utils = new Utils();
+                Session["cartCount"] = utils.cartCount(Convert.ToInt32(Session["userId"]).ToString);
+            }
+        }
+    }
+}
